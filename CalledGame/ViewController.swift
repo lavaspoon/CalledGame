b4ee160ae286bda4482df5486991897e8a5d2745
lavaspoon <lavaspoon@naver.com>
2022-08-10 00:23:14 +0900

    Initial Commit

---
+++ CalledGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalledGame
+//
+//  Created by spoon lava on 2022/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
